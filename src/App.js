9ff14c6e6bdce07ad7f6a8aaa47f2e92fe0ad1d8
Renzo <Renzomilanesi95@hotmail.com>
2022-11-15 20:51:16 -0300

    Acomodado de codigo BrowserRouter

--- src/App.js
+++ src/App.js
@@ -1,11 +1,23 @@
 
 import './App.css';
 import NavBar from "./components/Navbar/Navbar"
+import {BrowserRouter,Routes, Route} from "react-router-dom"
+import ErrorPage from "./components/RouteError/ErrorPage"
+import ItemListContainer from "./components/ItemListContainer/ItemListContainer"
+import ItemDetailContainer from './components/ItemDetail/ItemDetailContainer';
 function App() {
   return (
     
     <div className="App"> 
+    <BrowserRouter>
       <NavBar/>
+      <Routes>
+      <Route path="/" element={<ItemListContainer/>}/>
+      <Route path="/item/:id" element={<ItemDetailContainer/>}/>
+      <Route path="/category/:idCategory" element={<ItemListContainer/>}/>  
+      <Route path="*" element={<ErrorPage/>}/>   
+      </Routes>
+      </BrowserRouter>
     </div>
   );
 }
--- src/components/Navbar/Navbar.jsx
+++ src/components/Navbar/Navbar.jsx
@@ -2,15 +2,12 @@
 import CardWidget from "./CardWidget"
 import 'bootstrap/dist/css/bootstrap.min.css';
 import {Container,Nav,Navbar, }from 'react-bootstrap'
-import {BrowserRouter,Routes, Route ,Link} from "react-router-dom"
-import ErrorPage from "../RouteError/ErrorPage"
-import ItemListContainer from "../ItemListContainer/ItemListContainer"
-import ItemDetailContainer from '../ItemDetail/ItemDetailContainer';
+import {Link} from "react-router-dom"
+
 
 function ColorSchemesExample() {
   return (
     <>
-    <BrowserRouter>
       <Navbar bg="light" variant="light">
         <Container>
           <Navbar.Brand as={Link} to="/">HerreriaDiYaYa</Navbar.Brand>
@@ -18,21 +15,15 @@ function ColorSchemesExample() {
             <Nav.Link as={Link} to="/category/Interiores">Interiores</Nav.Link>
             <Nav.Link as={Link} to="/category/Banquetas">Banquetas</Nav.Link>
             <Nav.Link as={Link} to="/category/Patio">Patio</Nav.Link>
-            
-            <Nav.Link href="#pricing"><CardWidget/></Nav.Link>
+            <Nav.Link href="/"><CardWidget/></Nav.Link>
           </Nav>
         </Container>
         
       </Navbar>
       <div>
-      <Routes>
-      <Route path="/" element={<ItemListContainer/>}/>
-      <Route path="/item/:id" element={<ItemDetailContainer/>}/>
-      <Route path="/category/:idCategory" element={<ItemListContainer/>}/>  
-      <Route path="*" element={<ErrorPage/>}/>   
-      </Routes>
+      
       </div>
-      </BrowserRouter>
+      
     </>
   );
 }
